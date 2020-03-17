cd7e1f23021caf7a9a585ab470a0810622c097e9
kian behzad <kian.behzad@gmail.com>
2020-03-17 23:29:08 +0330

    [parsian_util] create the package and add general file for other nodes

---
+++ parsian_ssl/parsian_util/include/parsian_util/knowledge/general.h
@@ -0,0 +1,25 @@
+//
+// Created by kian behzad on 3/17/20.
+//
+
+#ifndef PARSIAN_UTIL_GENERAL_H
+#define PARSIAN_UTIL_GENERAL_H
+
+namespace knowledge
+{
+    class Field
+    {
+
+    };
+
+
+    class Robot
+    {
+        public:
+        static const int MAX_ROBOT_NUM = 12;
+
+    };
+
+}
+
+#endif //PARSIAN_UTIL_GENERAL_H
---
+++ parsian_ssl/parsian_util/src/knowledge/general.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kian behzad on 3/17/20.
+//
+
+#include "parsian_util/knowledge/general.h"
