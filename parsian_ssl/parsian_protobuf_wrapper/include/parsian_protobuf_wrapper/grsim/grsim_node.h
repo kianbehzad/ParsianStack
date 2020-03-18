1d0e0ea94d5589c0fca9a58c5d0398d572ec2c68
kian behzad <kian.behzad@gmail.com>
2020-03-18 18:44:26 +0330

    [grsim_node] implement world_model subscriber

--- parsian_ssl/parsian_protobuf_wrapper/include/parsian_protobuf_wrapper/grsim/grsim_node.h
+++ parsian_ssl/parsian_protobuf_wrapper/include/parsian_protobuf_wrapper/grsim/grsim_node.h
@@ -8,6 +8,7 @@
 #include <memory>
 
 #include "parsian_msgs/msg/parsian_robot_command.hpp"
+#include "parsian_msgs/msg/parsian_world_model.hpp"
 #include "parsian_util/knowledge/general.h"
 #include <string>
 
@@ -21,8 +22,10 @@ public:
     GrsimNode();
 
 private:
+    void worldmodel_callback(const parsian_msgs::msg::ParsianWorldModel::SharedPtr msg) const;
     void command_callback(const parsian_msgs::msg::ParsianRobotCommand::SharedPtr msg) const;
     rclcpp::Subscription<parsian_msgs::msg::ParsianRobotCommand>::SharedPtr command_subscription[knowledge::Robot::MAX_ROBOT_NUM];
+    rclcpp::Subscription<parsian_msgs::msg::ParsianWorldModel>::SharedPtr worldmodel_subscription;
 
 };
 
--- parsian_ssl/parsian_protobuf_wrapper/src/grsim/grsim_node.cpp
+++ parsian_ssl/parsian_protobuf_wrapper/src/grsim/grsim_node.cpp
@@ -2,11 +2,17 @@
 
 GrsimNode::GrsimNode() : Node("grsim_node")
 {
+    worldmodel_subscription = this->create_subscription<parsian_msgs::msg::ParsianWorldModel>("/world_model", 10, std::bind(&GrsimNode::worldmodel_callback, this, _1));
     for(int i{}; i < knowledge::Robot::MAX_ROBOT_NUM; i++)
         command_subscription[i] = this->create_subscription<parsian_msgs::msg::ParsianRobotCommand>("/agent_" + std::to_string(i) + "/command", 10, std::bind(&GrsimNode::command_callback, this, _1));
 }
 
 void GrsimNode::command_callback(const parsian_msgs::msg::ParsianRobotCommand::SharedPtr msg) const
 {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%d', '%d'", msg->kick_speed, knowledge::Robot::MAX_ROBOT_NUM);
+    RCLCPP_INFO(this->get_logger(), "robot_command: '%d', '%d'", msg->robot_id, msg->kick_speed);
+}
+
+void GrsimNode::worldmodel_callback(const parsian_msgs::msg::ParsianWorldModel::SharedPtr msg) const
+{
+    RCLCPP_INFO(this->get_logger(), "world_model: '%f', '%f'", msg->ball.pos.x, msg->ball.pos.y);
 }
