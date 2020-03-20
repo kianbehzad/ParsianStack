641ea02bc5b20cdf9823bb2ee134412719a90096
kian behzad <kian.behzad@gmail.com>
2020-03-20 21:25:30 +0330

    [parsian_util] fix export library bug

--- parsian_ssl/parsian_util/include/parsian_util/core/knowledge.h
+++ parsian_ssl/parsian_util/include/parsian_util/core/knowledge.h
@@ -2,13 +2,12 @@
 // Created by kian behzad on 3/20/20.
 //
 
-#ifndef PARSIAN_UTIL_ROBOT_H
-#define PARSIAN_UTIL_ROBOT_H
+#ifndef PARSIAN_UTIL_KNOWLEDG_H
+#define PARSIAN_UTIL_KNOWLEDG_H
 
 namespace knowledge
 {
     const int MAX_ROBOT_NUM = 12;
 }
 
-
-#endif //PARSIAN_UTIL_ROBOT_H
+#endif //PARSIAN_UTIL_KNOWLEDG_H
