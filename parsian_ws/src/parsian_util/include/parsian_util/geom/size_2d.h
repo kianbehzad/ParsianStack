b7cedce8b2f4cd2b811bd78ad80234ec25df348a
kian behzad <kian.behzad@gmail.com>
2020-04-05 20:13:04 +0430

    [geom] seperate size2d

--- parsian_ws/src/parsian_util/include/parsian_util/geom/size_2d.h
+++ parsian_ws/src/parsian_util/include/parsian_util/geom/size_2d.h
@@ -54,21 +54,14 @@ public:
     /*!
       \brief default constructor.
     */
-    Size2D()
-        : M_length(0.0)
-        , M_width(0.0) {
-    }
+    Size2D();
 
     /*!
       \brief constructor with variables
       \param length x range
       \param width y range
      */
-    Size2D(const double & length,
-           const double & width)
-        : M_length(std::fabs(length))
-        , M_width(std::fabs(width)) {
-    }
+    Size2D(const double & length, const double & width);
 
     /*!
       \brief assign new range directly.
@@ -76,72 +69,44 @@ public:
       \param width new Y range
       \return reference to itself
     */
-    const
-    Size2D & assign(const double & length,
-                    const double & width) {
-        M_length = std::fabs(length);
-        M_width = std::fabs(width);
-        return *this;
-    }
-
+    const Size2D & assign(const double & length, const double & width);
     /*!
       \brief set new X range
       \param length new X range
       \return reference to itself
     */
-    const
-    Size2D & setLength(const double & length) {
-        M_length = std::fabs(length);
-        return *this;
-    }
-
+    const Size2D & setLength(const double & length);
     /*!
       \brief set new Y range
       \param width new Y range
       \return reference to itself
     */
-    const
-    Size2D & setWidth(const double & width) {
-        M_width = std::fabs(width);
-        return *this;
-    }
+    const Size2D & setWidth(const double & width);
 
     /*!
       \brief get the value of X range
       \return value of X range
      */
-    const
-    double & length() const {
-        return M_length;
-    }
+    const double & length() const;
 
     /*!
       \brief get the value of Y range
       \return value of Y range
      */
-    const
-    double & width() const {
-        return M_width;
-    }
+    const double & width() const;
 
     /*!
       \brief get the length of diagonal line
       \return length of diagonal line
      */
-    double diagonal() const {
-        return std::sqrt(length() * length()
-                         + width() * width());
-    }
+    double diagonal() const;
 
     /*!
       \brief output values to stream.
       \param os reference to the output stream
       \return reference to the output stream
      */
-    std::ostream & print(std::ostream & os) const {
-        os << "(" << length() << ", " << width() << ")";
-        return os;
-    }
+    std::ostream & print(std::ostream & os) const;
 
 };
 
