39480301dc5cd1cd1fca27006bb1b88999ef5432
kian behzad <kian.behzad@gmail.com>
2020-04-07 21:01:05 +0430

    [worldmodel_node] add world model

--- parsian_ws/src/parsian_util/tools/launch/grsim_launch.py
+++ parsian_ws/src/parsian_util/tools/launch/grsim_launch.py
@@ -9,5 +9,6 @@ def generate_launch_description():
     
     grsim_node = Node(package='parsian_protobuf_wrapper', node_executable='grsim', node_name='grsim_node', parameters=[param_path])
     vision_node = Node(package='parsian_protobuf_wrapper', node_executable='vision', node_name='vision_node', parameters=[param_path])
+    worldmodel_node = Node(package='parsian_world_model', node_executable='worldmodel', node_name='worldmodel_node', parameters=[param_path])
     
-    return LaunchDescription([grsim_node, vision_node])
+    return LaunchDescription([grsim_node, vision_node, worldmodel_node])
